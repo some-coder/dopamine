327471130679ba03c612d4b68190181479470a93
Niels de Jong <n.a.de.jong@student.rug.nl>
2022-03-24 02:06:41 +0100

    Work further on the `ObjectSaliencyMapAssistant`
    Basic object detection works now, at least in Ms. Pac-Man. The next step
    is to associate object BBs through the four time points of the buffer.
    Once this is done, we can proceed with removing individual objects from
    the scene.

--- dopamine/discrete_domains/util.py
+++ dopamine/discrete_domains/util.py
@@ -16,8 +16,9 @@ from dataclasses import dataclass
 import matplotlib.pyplot as plt
 import cv2
 from PIL import Image
+import re
 
-from typing import Dict, List, Optional, Tuple
+from typing import Any, Dict, List, Optional, Set, Tuple
 
 
 @dataclass
@@ -116,6 +117,7 @@ class ObjectBoundingBox:
     y_min: int
     y_max: int
     name: Optional[str] = None
+    buf_entry: Optional[int] = None
 
 
 class ObjectSaliencyMapAssistant:
@@ -132,6 +134,19 @@ class ObjectSaliencyMapAssistant:
     SUPPORTED_GAMES: List[str] = ['Pong', 'MsPacman']
     DILATE_KERNEL = \
         cv2.getStructuringElement(shape=cv2.MORPH_RECT, ksize=(3, 3))
+    
+    # maps grayscale tones to names of instances withina shared category
+    PONG_PADDLE_NAMES: Dict[int, str] = \
+        {147: 'green-paddle', 148: 'orange-paddle'}
+    MS_PACMAN_GHOST_NAMES: Dict[int, str] = \
+        {
+            110: 'blinky-red',
+            131: 'clyde-yellow',
+            151: 'inky-blue',
+            132: 'pinky-pink'
+        }
+    MS_PACMAN_WALL_NAMES: Dict[int, str] = \
+        {146: 'pink', 121: 'gray', 170: 'blue', 132: 'green'}
 
     def __init__(self, game: str) -> None:
         assert DQN_USE_OBJECTS
@@ -195,17 +210,41 @@ class ObjectSaliencyMapAssistant:
                 bb_arg_val = input(f'{bb_arg}? ')
             bb_args[bb_arg] = int(bb_arg_val)
         bb_args['name'] = input(f'(Name?) ')
+        bb_args['buf_entry'] = input(f'(Index of buffer entry?) ')
         plt.close()
         return ObjectBoundingBox(**bb_args)   
+
+    def automatic_obb_name(
+            self,
+            template_name: str,
+            x_min: int,
+            y_min: int,
+            img: np.ndarray) -> str:
+        assert DQN_SCREEN_MODE != DQNScreenMode.RGB  # won't work
+        if self.game == 'Pong':
+            if template_name == 'paddle-piece-wide':
+                return self.PONG_PADDLE_NAMES[int(img[0, 1])]
+            elif template_name == 'ball-padded':
+                return 'ball'
+        elif self.game == 'MsPacman':
+            if template_name == 'blinky-red-padded':
+                return self.MS_PACMAN_GHOST_NAMES[int(img[-4, 3])]
+            elif template_name == 'pellet-padded':
+                return f'pellet-y-{y_min}-x-{x_min}'
+            elif template_name == 'power-pellet-padded':
+                return f'power-pellet-y-{y_min}-x-{x_min}'
+        raise ValueError(
+            f'Couldn\'t find a name for \'{template_name}\'!'
+        )
 
     def automatically_designate_object_bounding_box(
             self,
             name: str,
-            postfix: Optional[str] = None) -> List[ObjectBoundingBox]:
+            buf_entry: int) -> List[ObjectBoundingBox]:
         if name not in self.objects_map.keys():
-            print(
-                'No such key. Try one of the following:' +
-                ', '.join(f'\'{self.objects_map.keys()}\'')
+            raise KeyError(
+                'No such key. Try one of the following: ' +
+                ', '.join([f"'{k}'" for k in self.objects_map.keys()])
             )
         template, threshold, mask = self.objects_map[name]
         if mask is not None:
@@ -223,22 +262,65 @@ class ObjectSaliencyMapAssistant:
         )
         locs = np.where(sq_diff < threshold)
         bbs: List[ObjectBoundingBox] = []
-        for index, (y, x) in enumerate(zip(*locs)):
+        for _, (y, x) in enumerate(zip(*locs)):
+            sub_img = img[y:(y + template.shape[0]), x:(x + template.shape[1])]
             bbs.append(
                 ObjectBoundingBox(
                     x_min=x,
                     x_max=x + template.shape[1],
                     y_min=y,
                     y_max=y + template.shape[0],
-                    name=f'{name}-{index}{"" if postfix is None else "-{postfix}"}'
+                    name=self.automatic_obb_name(name, x, y, sub_img),
+                    buf_entry=buf_entry
                 )
             )
         return bbs
 
     @staticmethod
+    def names_to_obb_map(obb_list: List[ObjectBoundingBox]) -> \
+            Dict[str, List[ObjectBoundingBox]]:
+        obb_names: Set[str] = set(entry.name for entry in obb_list)
+        obb_map: Dict[str, List[ObjectBoundingBox]] = \
+            {key: [] for key in obb_names}
+        for entry in obb_list:
+            obb_map[entry.name] = entry
+        return obb_map
+
+    @staticmethod
+    def map_keys_starting_with(map: Dict[str, Any], start: str) -> List[str]:
+        return [key for key in map.keys() if re.search(f'^({start})', key)]
+
+    def cleaned_obb_map(self, obb_map: Dict[str, List[ObjectBoundingBox]]) -> \
+            Dict[str, List[ObjectBoundingBox]]:
+        if self.game == 'Pong':
+            raise NotImplementedError(f'Pong is not yet implemented!')
+        elif self.game == 'MsPacman':
+            ghost_re: str = '|'.join(('blinky', 'clyde', 'inky', 'pinky'))
+            for key in self.map_keys_starting_with(obb_map, ghost_re):
+                reduction: Dict[int, Optional[ObjectBoundingBox]] = \
+                    {bi: None for bi in tuple(range(4))}
+                first_non_empty: Optional[ObjectBoundingBox] = None
+                for entry in obb_map[key]:
+                    if reduction[entry.buf_entry] is None:
+                        reduction[entry.buf_entry] = entry
+                    elif first_non_empty is None:
+                        first_non_empty = entry
+                for key in [k for k, v in reduction.items() if v is None]:
+                    reduction[key] = first_non_empty
+                    reduction[key].buf_entry = key  # fix buffer indexing
+                obb_map[key] = reduction
+            for key in self.map_keys_starting_with(obb_map, 'pellet'):
+                if len(obb_map[key]) != 4:
+                    del obb_map[key]  # no occlusion at any step
+            for key in self.map_keys_starting_with(obb_map, 'power-pellet'):
+                obb_map[key] = [obb_map[key][0]] * 4  # ignore the flashing
+                for index, entry in enumerate(obb_map[key]):
+                    entry.buf_entry = index  # fix buffer indexing
+        return obb_map
+
+    @staticmethod
     def normal_treatment_applied_to_object_channel(
             raw: np.ndarray,
-            index: int,
             threshold: float,
             sq_diff: np.ndarray,
             template: np.ndarray) -> np.ndarray:
@@ -247,8 +329,7 @@ class ObjectSaliencyMapAssistant:
         for y, x in zip(*locs):
             out[
                 y:(y + template.shape[0]),
-                x:(x + template.shape[1]),
-                index
+                x:(x + template.shape[1])
             ] = 255
         return out
 
@@ -264,19 +345,21 @@ class ObjectSaliencyMapAssistant:
     @staticmethod
     def special_treatment_applied_to_object_channel(
             raw: np.ndarray,
-            index: int,
             thresholds: Tuple[float, float, float, float],
             sq_diff: np.ndarray) -> np.ndarray:
         locs: Optional[Tuple[np.ndarray, np.ndarray]] = None
-        for thr in thresholds:
-            if raw[1, 0] == thr:
-                locs = np.where(sq_diff < thr)
+        wall_enum = enumerate(
+            ObjectSaliencyMapAssistant.MS_PACMAN_WALL_NAMES.keys()
+        )
+        for index, wall_thr in wall_enum:
+            if raw[1, 0] == wall_thr:
+                locs = np.where(sq_diff < thresholds[index])
                 break
         assert locs is not None  # threshold must've been found
         out = np.zeros(shape=raw.shape[:2])
-        out[locs[0], locs[1], index] = 255
-        out[..., index] = cv2.dilate(
-            src=out[..., index],
+        out[locs[0], locs[1]] = 255
+        out = cv2.dilate(
+            src=out,
             kernel=ObjectSaliencyMapAssistant.DILATE_KERNEL,
             iterations=1
         )
@@ -294,12 +377,12 @@ class ObjectSaliencyMapAssistant:
             if self.object_channel_requires_special_treatment(self.game, idx):
                 obj_chn[..., idx] = \
                     self.special_treatment_applied_to_object_channel(
-                        raw, idx, thr, sq_diff
+                        raw, thr, sq_diff
                     )
             else:
                 obj_chn[..., idx] = \
                     self.normal_treatment_applied_to_object_channel(
-                        raw, idx, thr, sq_diff, tpl
+                        raw, thr, sq_diff, tpl
                     )
         return obj_chn
 
@@ -319,23 +402,3 @@ class ObjectSaliencyMapAssistant:
                 raw_copy = np.expand_dims(raw_copy, axis=-1)
             out = np.concatenate((raw_copy, out), axis=-1)
         return out
-
-    def inputs_with_single_objects_removed(
-            self,
-            obbs: List[ObjectBoundingBox]) -> Dict[str, List[np.ndarray]]:
-        inputs: Dict[str, List[np.ndarray]] = {}
-        for index, obb in enumerate(obbs):
-            inp: List[np.ndarray] = []
-            for buf_entry in self.buf.buf:
-                inp.append(
-                    self.input_with_single_object_removed(
-                        obb, buf_entry
-                    )
-                )
-            if obb.name is None:
-                print(f'Skipping object BB with index {index}: no name.')
-            elif obb.name in inputs.keys():
-                print(f'Skipping object BB with index {index}: duplicate.')
-            else:
-                inputs[obb.name] = inp
-        return inputs
